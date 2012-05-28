d451ee213652812a1de6fea3a98b15b5d1c42880
chris <chris@westnet.com>
2012-05-28 15:17:03 -0400

    Add methods to return emotions, urges, and emotion types as arrays

--- lib/DbtDiary/Util.php
+++ lib/DbtDiary/Util.php
@@ -10,6 +10,9 @@
 
 class DbtDiary_Util
 {
+
+
+    
     public function initListValues($list, $firstnull = null)
     {
             $temp = array();
@@ -19,6 +22,41 @@ class DbtDiary_Util
                 $temp[] = array('text' => $v, 'value' => $v);
             }
             return $temp;
+    }
+    
+    public function getEmotions()
+    {
+        return array('hurt', 'good', 'tense', 'miserable', 'panic', 
+        'overwhelmed', 'angry', 'sad', 'hopeful', 'alone', 'distracted', 
+        'bad', 'guilty', 'unreal');
+    }
+    public function getEmotionTypes()
+    {
+        return array(
+        'hurt' => 'N',
+        'good' => 'P',
+        'tense' => 'N',
+        'miserable' => 'N',
+        'panic' => 'N',
+        'overwhelmed' => 'N',
+        'angry' => 'N',
+        'sad' => 'N',
+        'hopeful' => 'P',
+        'alone' => 'N',
+        'distracted' => 'N',
+        'bad' => 'N',
+        'guilty' => 'N',
+        'unreal' => 'N',
+        );
+
+
+    }
+
+    public function getUrges()
+    {
+        return array('injure', 'kill', 'meds', 'skip', 
+                     'binge', 'purge', 'alcohol', 'drugs');
+
     }
 
     
